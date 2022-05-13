ca1e8f36004d1e7c742bde6c088e312c8f0d97f7
Ciprian <cozma_ciprian19@yahoo.com>
2022-05-13 17:28:35 +0300

    uploaded quote generator project

---
+++ script.js
@@ -0,0 +1,54 @@
+const quoteContainer = document.getElementById("quote-container")
+const quoteText = document.getElementById("quote")
+const authorText = document.getElementById("author")
+const newQuoteBtn = document.getElementById('new-quote')
+const loader = document.getElementById('loader')
+
+let apiQuotes = []
+
+function loading() {
+    loader.hidden = false;
+    quoteContainer.hidden = true
+}
+
+function complete() {
+    quoteContainer.hidden = false
+    loader.hidden = true
+}
+
+function newQoute() {
+    loading()
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)]
+
+    if(!quote.author) {
+        authorText.textContent = "Unknown"
+    } else {
+        authorText.textContent = quote.author
+    }
+
+    if(quote.text.length > 100) {
+        quoteText.classList.add("long-quote")
+    } else {
+        quoteText.classList.remove("long-quote")
+
+    }
+
+    quoteText.textContent = quote.text
+    complete()
+}
+
+newQuoteBtn.addEventListener("click", newQoute)
+
+async function getQuotes() {
+    loading()
+    const apiUrl = 'https://type.fit/api/quotes'
+    try {
+        const response = await fetch(apiUrl)
+        apiQuotes = await response.json()
+        newQoute();
+    } catch(err) {
+        console.error(err)
+    }
+}
+
+getQuotes()
